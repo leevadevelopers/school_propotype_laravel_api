4156eec275a1a7ce2968906ecbd42164b8c7824b
Cripton Baloi <72799525+258CriptonBaloi@users.noreply.github.com>
2024-07-30 13:39:40 +0200

    school_prototype

---
+++ database/migrations/2024_07_30_081856_create_courses_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Category;
+use App\Models\Teacher;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->enum('status', ['completed', 'pending']);
+            $table->string('time');
+            $table->foreignId(Teacher::class);
+            $table->foreignIdFor(Category::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
---
+++ database/migrations/2024_07_30_081903_create_chapters_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Course;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('chapters', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->enum('status', ['completed', 'pending']);
+            $table->integer('position');
+            $table->foreignIdFor(Course::class);
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chapters');
+    }
+};
